90b3f3a8dcf58b6c103e0d9a9bbfd9a320e16508
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2012-09-10 22:19:52 -0700

    Added a Nodepaper wrapper. Loosened the coupling between Server and Theme. Added filesystem support for Themes.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     Article: require('./lib/article'),
     Collection: require('./lib/collection'),
     Theme: require('./lib/theme'),
-    Server: require('./lib/server')
+    Server: require('./lib/server'),
+    Nodepaper: require('./lib/nodepaper')
 };
