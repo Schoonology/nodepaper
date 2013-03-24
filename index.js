dec1fc0f08bc6f2b2b1c60f92f1e1faf71418f48
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-03-23 17:15:45 -0700

    Improved config loading and loadability by dependents.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var Server = require('./lib/server')
+
 if (require.main === module) {
   require('./bin/nodepaper')
+} else {
+  module.exports = {
+    Server: Server,
+    createServer: Server.createServer
+  }
 }
